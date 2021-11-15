3936da49524baa856395f11ca187bde8e7c99352
DennyPlank <DennyMouzon@gmail.com>
2021-11-15 13:37:59 -0800

    intial setup, no changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+
+page.create(title: 'Page 1', author: 'Jim', body: 'yo')
+
+10.times do
+  Page.create(
+    title: Faker::Hacker.abbreviation,
+    body: Faker::Hacker.say_something_smart,
+    author: Faker::Name.name,
+)
+end
